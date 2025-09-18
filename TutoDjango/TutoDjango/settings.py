f3764963caacf888c0f7accb8f0ea2f35281383c
jules-gruson-delannoy <jules.gruson-delannoy@etu.univ-orleans.fr>
2025-09-18 10:01:47 +0200

    fin td2

--- TutoDjango/TutoDjango/settings.py
+++ TutoDjango/TutoDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'monApp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'TutoDjango.urls'
@@ -69,6 +71,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TutoDjango.wsgi.application'
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,21 +1,55 @@
 from django.contrib import admin
-from .models import Produit, Categorie, Statut, Rayon, Contenir
+from decimal import Decimal, ROUND_HALF_UP
+from .models import Produit, Categorie, Statut, Rayon
 
-class ProduitAdmin(admin.ModelAdmin):
-    model = Produit
-    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabrication", "categorie", "statut"]
-    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabrication"]
 
 class ProduitInline(admin.TabularInline):
     model = Produit
-    extra = 1 # nombre de lignes vides par défaut
+    extra = 1
 
 class CategorieAdmin(admin.ModelAdmin):
-    model = Categorie
     inlines = [ProduitInline]
+
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('OnLine', 'En ligne'),
+            ('OffLine', 'Hors ligne'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(statut_id=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(statut_id=0)
+
+def set_produit_online(modeladmin, request, queryset):
+    queryset.update(statut_id=1)
+set_produit_online.short_description = "Mettre en ligne"
+
+def set_produit_offline(modeladmin, request, queryset):
+    queryset.update(statut_id=0)
+set_produit_offline.short_description = "Mettre hors ligne"
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "prixTTCProd", "dateFabrication", "categorie", "statut"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabrication"]
+    radio_fields = {"statut": admin.VERTICAL}
+    search_fields = ("intituleProd", "dateFabrication")
+    list_filter = (ProduitFilter,)
+    date_hierarchy = "dateFabrication"
+    ordering = ("-dateFabrication",)
+    actions = [set_produit_online, set_produit_offline]
+
+    def prixTTCProd(self, instance):
+        return (instance.prixUnitaireProd * Decimal('1.20')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    prixTTCProd.short_description = "Prix TTC"
+    prixTTCProd.admin_order_field = "prixUnitaireProd"
 
 admin.site.register(Produit, ProduitAdmin)
 admin.site.register(Categorie, CategorieAdmin)
 admin.site.register(Statut)
 admin.site.register(Rayon)
-admin.site.register(Contenir)
